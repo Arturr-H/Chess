b8d89943276a7bb597aaaa1f01a19d380794890d
Artur <artur.h.0427@gmail.com>
2023-02-14 20:49:18 +0100

    Initialize `actix-web` router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+/* Global allowances */
+#![allow(
+    dead_code
+)]
+
+/* Imports */
+use actix_web::{ App, HttpServer };
+use actix_files;
+
+/* Modules */
+mod routes;
+mod board;
+mod traits;
+
+/* Main */
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    // let appdata = Data::new(Mutex::new(appdata::AppData::from_file()));
+    HttpServer::new(move || {
+        App::new()
+            /* Set maximum payload size to 32MiB */
+            // .app_data(web::PayloadConfig::new(1 << 25))
+            // .app_data(Data::clone(&appdata))
+            
+
+            /* Static files */
+            .service(routes::index)
+            .service(actix_files::Files::new("/static", "./frontend/src/").index_file(""))
+
+            /* Static files */
+            .service(routes::js_dist)
+            .service(routes::js_htmx)
+    })
+    .workers(12)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
