729d29d4e4a2e401edf929148b8fa68be4866146
Artur <artur.h.0427@gmail.com>
2023-02-16 00:36:36 +0100

    Implement `is_checking_king` for `Knight`

--- src/chess-pieces/knight.rs
+++ src/chess-pieces/knight.rs
@@ -14,16 +14,16 @@ impl PieceMethods for Knight {
     fn get_moves_local(&self) -> Vec<(i8, i8)> {
         vec![
             /* Top */
-            (-1, 2), (1, 2),
+            (-1, -2), (1, -2),
 
             /* Right */
-            (2, 1), (2, -1),
+            (2, -1), (2, 1),
 
             /* Bottom */
-            (1, -2), (-1, -2),
+            (1, 2), (-1, 2),
 
             /* Left */
-            (-2, 1), (-2, -1),
+            (-2, -1), (-2, 1),
         ]
     }
 
@@ -34,4 +34,41 @@ impl PieceMethods for Knight {
 
     /* Getters */
     fn color(&self) -> Color { self.color }
+
+    /* If is checking opposing king */
+    fn is_checking_king(&self, color_of_king: Color, x: i8, y: i8, board: &crate::board::Board) -> bool {
+        let places = &[
+            /* Top */
+            (x - 1, y - 2), (x + 1, y - 2),
+
+            /* Right */
+            (x + 2, y - 1), (x + 2, y + 1),
+
+            /* Bottom */
+            (x + 1, y + 2), (x - 1, y + 2),
+
+            /* Left */
+            (x - 2, y - 1), (x - 2, y + 1),
+        ];
+
+        for (new_x, new_y) in places {
+            
+            /* Check item */
+            match board.get(*new_x, *new_y) {
+                Some(e) => {
+                    match e {
+                        Piece::King(e) => {
+                            if e.color() == color_of_king {
+                                return true
+                            }
+                        },
+                        _ => { continue; }
+                    }
+                },
+                None => ()
+            }
+        }
+
+        false
+    }
 }
