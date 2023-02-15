5dcf8f0424014cacd3bf4e42f594cfc168ac9a8e
Artur <artur.h.0427@gmail.com>
2023-02-15 18:10:30 +0100

    Create `piece.rs` - to handle all diffrent pieces easily

---
+++ src/piece.rs
@@ -0,0 +1,47 @@
+/* Imports */
+use crate::{
+    bundle::{
+        rook::Rook,
+        horse::Horse,
+        king::King,
+        queen::Queen,
+        bishop::Bishop,
+        pawn::Pawn
+    },
+    board::Board, traits::PieceMethods
+};
+
+/// The pieces
+#[derive(Clone, Copy)]
+pub enum Piece {
+    Rook(Rook),
+    Horse(Horse),
+    King(King),
+    Queen(Queen),
+    Bishop(Bishop),
+    Pawn(Pawn),
+}
+
+/* Method implementations */
+impl Piece {
+    /// Get piece methods
+    pub fn get_generic_inner(&self) -> &dyn PieceMethods {
+        match self {
+            Self::Rook(e) => e,
+            Self::Horse(e) => e,
+            Self::King(e) => e,
+            Self::Queen(e) => e,
+            Self::Bishop(e) => e,
+            Self::Pawn(e) => e,
+        }
+    }
+
+    /// Get color
+    pub fn color(&self) -> Color {
+        self.get_generic_inner().color()
+    }
+}
+
+/// Piece color
+#[derive(Clone, Copy, PartialEq)]
+pub enum Color { White, Black }
