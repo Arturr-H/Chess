4f0edec396556aaf6e08f700c4806795e4ebd5f8
Artur <artur.h.0427@gmail.com>
2023-02-15 21:50:07 +0100

    Create `queen.rs` tests

---
+++ tests/queen.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+    use chess::{ board::Board, bundle::queen::Queen, piece::Color, traits::PieceMethods };
+
+    #[test]
+    #[should_panic]
+    fn move_() -> () {
+        let mut board = Board::new();
+
+        /* Queens cant initially move, therefore this test should panic */
+        board.move_piece_to_coordinate((3, 0), (3, 1)).unwrap();
+    }
+
+    #[test]
+    fn white_moves() -> () {
+        let queen_w = Queen { color: Color::White };
+        let queen_b = Queen { color: Color::Black };
+
+        assert_eq!(queen_w.get_moves_local().contains(&(1, 1)), true);
+        assert_eq!(queen_w.get_moves_local().contains(&(-1, 1)), true);
+        assert_eq!(queen_b.get_moves_local().contains(&(0, 1)), false);
+    }
+}
