64a3f278c27a83f804baeae5a6d58f864c236c13
Artur <artur.h.0427@gmail.com>
2023-02-15 22:52:52 +0100

    Update knight movements to begin with white

--- tests/knight.rs
+++ tests/knight.rs
@@ -13,9 +13,7 @@ mod tests {
     #[test]
     fn move_() -> () {
         let mut board = Board::new();
-        dbg!(board.get(1, 0));
-        board.move_piece_to_coordinate((1, 0), (2, 2)).unwrap();
-        board.move_piece_to_coordinate((1, 0), (0, 2)).unwrap();
+        board.move_piece_to_coordinate((1, 7), (2, 5)).unwrap();
     }
 
     #[test]
