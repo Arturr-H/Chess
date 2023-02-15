56aed4a4b5ab5896e8b2051f5c77d426d3180cc1
Artur <artur.h.0427@gmail.com>
2023-02-16 00:36:06 +0100

    Implement `is_checking_king` for `King`

--- src/chess-pieces/king.rs
+++ src/chess-pieces/king.rs
@@ -33,4 +33,33 @@ impl PieceMethods for King {
 
     /* Getters */
     fn color(&self) -> Color { self.color }
+
+    /* Check if cheking opposing king */
+    fn is_checking_king(&self, color_of_king: Color, x: i8, y: i8, board: &crate::board::Board) -> bool {
+        let places = &[
+            (x-1, y-1), (x, y-1), (x+1, y-1),
+            (x-1, y)  ,           (x+1, y),
+            (x-1, y+1), (x, y+1), (x+1, y+1)
+        ];
+
+        for (new_x, new_y) in places {
+            
+            /* Check item */
+            match board.get(*new_x, *new_y) {
+                Some(e) => {
+                    match e {
+                        Piece::King(e) => {
+                            if e.color() == color_of_king {
+                                return true
+                            }
+                        },
+                        _ => { continue; }
+                    }
+                },
+                None => ()
+            }
+        }
+        
+        false
+    }
 }
