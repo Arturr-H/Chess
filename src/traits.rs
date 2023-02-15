0540bfc43d21829ffae155fb7644e8de9d909752
Artur <artur.h.0427@gmail.com>
2023-02-15 18:10:03 +0100

    Rename `ChessPiece` trait to `PieceMethods`, and add new methods

--- src/traits.rs
+++ src/traits.rs
@@ -1,27 +1,18 @@
 /* Imports */
-use crate::board::Board;
-
-/// Piece type
-pub enum PieceType {
-    White,
-    Black,
-    Empty
-}
-
-/// Method impl
-impl PartialEq for PieceType {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (PieceType::White, PieceType::White) => true,
-            (PieceType::Black, PieceType::Black) => true,
-            (PieceType::Empty, PieceType::Empty) => true,
-            _ => false,
-        }
-    }
-}
+use crate::{
+    bundle::{
+        rook::Rook,
+        horse::Horse,
+        king::King,
+        queen::Queen,
+        bishop::Bishop,
+        pawn::Pawn
+    },
+    board::Board, piece::{Color, Piece}
+};
 
 /// Pre-defined functions for all chess pieces
-pub trait ChessPiece {
+pub trait PieceMethods {
     /// If a chess piece can move to tile, the `to`
     /// param is a tuple of two signed integers 
     /// which are the "move request". Will check if
@@ -31,11 +22,11 @@ pub trait ChessPiece {
         /* If move is physically possible by that piece */
         if Self::get_moves_local().contains(&move_) {
             if let Some(piece) = board.get(move_.0, move_.1) {
-                let type_ = piece.piece_type();
+                let type_ = piece.color();
 
                 // TODO: look out for if king is checked
 
-                if type_ != self.piece_type() {
+                if type_ != self.color() {
                     true
                 }else {
                     false
@@ -51,6 +42,9 @@ pub trait ChessPiece {
     /// Get all possible moves on the board, relative to the piece
     fn get_moves_local() -> Vec<(i8, i8)> where Self: Sized;
 
-    /// Get color of piece
-    fn piece_type(&self) -> &PieceType;
+    /// Get the color
+    fn color(&self) -> Color;
+
+    /// Constructor
+    fn new<'a>(color: Color) -> Piece where Self: Sized;
 }
