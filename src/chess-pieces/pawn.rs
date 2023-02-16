4b72d8750fd4a7babe171a00c1a8e382793888a0
Artur <artur.h.0427@gmail.com>
2023-02-16 13:27:16 +0100

    Add field `has_moved` to pawn

--- src/chess-pieces/pawn.rs
+++ src/chess-pieces/pawn.rs
@@ -4,24 +4,36 @@ use crate::{ traits::PieceMethods, piece::{ Color, Piece, invert_local_moves }, 
 /* Pawn */
 #[derive(Clone, Copy, Debug)]
 pub struct Pawn {
-    pub color: Color
+    pub color: Color,
+
+    /* User for looking if pawn can do the "double" leap or whatever you call that */
+    pub has_moved: bool
 }
 
 /* Constants */
 const LOCAL_MOVES:&[(i8, i8)] = &[(0, 1)];
+const LOCAL_MOVES_WITH_LEAP:&[(i8, i8)] = &[(0, 1), (0, 2)];
 
 /* Method implementations */
 impl PieceMethods for Pawn {
     fn get_moves_local(&self) -> Vec<(i8, i8)> {
-        if self.color() == Color::White {
-            invert_local_moves(LOCAL_MOVES)
+        if self.has_moved {
+            if self.color() == Color::White {
+                invert_local_moves(LOCAL_MOVES)
+            }else {
+                LOCAL_MOVES.to_vec()
+            }
         }else {
-            LOCAL_MOVES.to_vec()
+            if self.color() == Color::White {
+                invert_local_moves(LOCAL_MOVES_WITH_LEAP)
+            }else {
+                LOCAL_MOVES_WITH_LEAP.to_vec()
+            }
         }
     }
     /* Constructor */
     fn new<'a>(color: Color) -> Piece where Self: Sized {
-        Piece::Pawn(Self { color })
+        Piece::Pawn(Self { color, has_moved: false })
     }
 
     /* Getters */
