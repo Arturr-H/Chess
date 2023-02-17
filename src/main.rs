af89a626ee70abe9d6f97ac77def68861faf7137
Artur <artur.h.0427@gmail.com>
2023-02-17 17:19:24 +0100

    Include service `move_`

--- src/main.rs
+++ src/main.rs
@@ -14,54 +14,27 @@ pub mod piece;
 pub mod bundle;
 
 /* Imports */
-use actix_web::{ App, HttpServer };
+use std::{io::{Read, stdin, stdout, Write}, sync::Mutex};
+use actix_web::{ App, HttpServer, web::Data };
 use actix_files;
 use chess::{bundle::{rook::Rook, pawn::Pawn, bishop::Bishop, knight::Knight, king::King, queen::Queen}, traits::PieceMethods, board::{Board, Tile}};
 
 /* Main */
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let b = chess::piece::Color::Black;
-    let w = chess::piece::Color::White;
-    let non = Tile::Empty;
-    let piec = Tile::Piece;
-
-    let mut board = Board::from(
-        vec![
-            vec![ non, non, non, non, piec(King::new(b)), piec(Bishop::new(b)), piec(Knight::new(b)), piec(Rook::new(b)) ],
-            // vec![ piec(Pawn::new(b)); 8 ],
-            vec![ non; 8 ],
-            vec![ non; 8 ],
-            vec![ non; 8 ],
-            vec![ non; 8 ],
-            vec![ non; 8 ],
-            vec![ non; 8 ],
-            // vec![ piec(Pawn::new(w)); 8 ],
-            vec![ piec(Rook::new(w)), piec(Knight::new(w)), piec(Bishop::new(w)), piec(Queen::new(w)), piec(King::new(w)), piec(Bishop::new(w)), piec(Knight::new(w)), piec(Rook::new(w)) ],
-        ]
-    );
-    println!("{:?}", board);
-    println!("is white check: {:?}", board.is_in_check(chess::piece::Color::White));
-    println!("is black check: {:?}", board.is_in_check(chess::piece::Color::Black));
-    board.move_piece_to_coordinate((3, 7), (0, 4)).unwrap();
-    println!("{:?}", board);
-    println!("is white check: {:?}", board.is_in_check(chess::piece::Color::White));
-    println!("is black check: {:?}", board.is_in_check(chess::piece::Color::Black));
-
-    // let appdata = Data::new(Mutex::new(appdata::AppData::from_file()));
+    let appdata = Data::new(Mutex::new(board::Board::new()));
     HttpServer::new(move || {
         App::new()
-            /* Set maximum payload size to 32MiB */
-            // .app_data(web::PayloadConfig::new(1 << 25))
-            // .app_data(Data::clone(&appdata))
+            .app_data(Data::clone(&appdata))
 
             /* Static files */
-            .service(routes::index)
             .service(actix_files::Files::new("/static", "./frontend/src/").index_file(""))
-
-            /* Static files */
+            .service(routes::index)
             .service(routes::js_dist)
             .service(routes::js_htmx)
+            
+            .service(routes::move_)
+
     })
     .workers(12)
     .bind(("127.0.0.1", 8080))?
